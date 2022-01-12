238b4f36ae1e0a4823c335b12bfbf24d5f0b0197
jimbo <jimbo@DESKTOP-MTVP7BL>
2022-01-12 09:50:51 +0100

    Added and implemented MultiWordCounter class

---
+++ lab1/src/textproc/MultiWordCounter.java
@@ -0,0 +1,29 @@
+package textproc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MultiWordCounter implements TextProcessor {
+	private Map<String, Integer> wordMap = new HashMap<String, Integer>();
+
+	public MultiWordCounter(String[] words) {
+		for (int i = 0; i < words.length; i++) {
+			wordMap.put(words[i], 0);
+		}
+	}
+
+	public void process(String w) {
+
+		if (wordMap.containsKey(w)) {
+			wordMap.put(w, wordMap.get(w) + 1);
+		}
+
+	}
+
+	public void report() {
+		for (String key : wordMap.keySet()) {
+			System.out.println(key + ": " + wordMap.get(key));
+		}
+	}
+
+}
